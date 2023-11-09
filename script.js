263eafe025d9a55115227c42a7ad5db4b6cb9648
shatalov1903 <1903shatalov@gmail.com>
2023-11-09 15:38:13 +0200

    commit

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function navToggle() {
 	document.getElementById("header-ul").classList.toggle("header__ul_active");
 }
-
 function navToggle() {
-	document.getElementById("aside-ul").classList.toggle("aside__ul_active");
+	document.getElementById("aside").classList.toggle("aside_active");
 }
